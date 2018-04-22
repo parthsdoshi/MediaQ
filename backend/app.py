3ceb8866f8003977e11718db1979b4926dfaf97f
Parth Doshi <doshi.parth9@gmail.com>
2018-04-22 01:53:40 -0400

    create app.py based on doc

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,14 @@ import os, time, random, string, json
 from flask import Flask, send_from_directory, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
 import redis
+# from rejson import Client, Path
+import time
+
+import socket_constants as constants
 
 app = Flask(__name__, static_folder='/build')
 socketio = SocketIO(app)
-database = redis.Redis(host='database', port=6379)
+database = redis.StrictRedis(host='database', port=6379)
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
@@ -19,28 +23,67 @@ def serve(path):
         else:
             return send_from_directory('/build', 'index.html')
 
-def get(key):
+def get(key, path):
     retries = 5
     while True:
         try:
-            return database.get(key)
+            return json.loads(database.execute_command('JSON.GET', key, path))
         except redis.exceptions.ConnectionError as exc:
             if retries == 0:
                 raise exc
             retries -= 1
             time.sleep(0.5)
+        except redis.exceptions.ResponseError as exc:
+            return None
 
-def set(key, value):
+def set(key, path, value):
     retries = 5
     while True:
         try:
-            return database.set(key, value)
+            return database.execute_command('JSON.SET', key, path, json.dumps(value))
         except redis.exceptions.ConnectionError as exc:
             if retries == 0:
                 raise exc
             retries -= 1
             time.sleep(0.5)
+        except redis.exceptions.ResponseError as exc:
+            return None
 
+def arrAppend(key, path, value):
+    retries = 5
+    while True:
+        try:
+            return database.execute_command('JSON.ARRAPPEND', key, path, json.dumps(value))
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+        except redis.exceptions.ResponseError as exc:
+            return None
+
+def tryDatabaseCommand(command, *args):
+    retries = 5
+    while True:
+        try:
+            return database.execute_command(command, *args)
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+        except redis.exceptions.ResponseError as exc:
+            return None
+
+
+# print(json.loads(database.execute_command('JSON.GET', "foo", "ans")))
+# set("foo", ".", "{}");
+# set("foo", "ans", "34");
+# print(json.loads(get("foo", "ans")));
+
+# initialize queues
+if (get('queues', '') == None):
+    set('queues', '.', {})
 
 # just some info about the socketio library...
 # calling just 'emit' sends back to the sender
@@ -50,128 +93,125 @@ def set(key, value):
 
 # TL;DR: socketio functions are NOT context aware
 
-@socketio.on('create')
+@socketio.on(constants.CREATE)
 def handle_create(data):
-    # print(data)
-
-    displayName = data['displayName']
+    displayName = data['data']
 
     random_id = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
 
-    while(get(random_id) != None):
+    while(get('queues', random_id) != None):
         random_id = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
 
     qID = random_id
     room = qID
 
     queue_info = {
-            'queue': [],
+            'queue': {},
             'connected_users': [{
                 'sid': request.sid,
                 'displayName': displayName}]
             }
-    set(qID, json.dumps(queue_info))
+    set("queues", qID, queue_info)
 
     join_room(room)
-
-    # random_id should always equal room here
-    emit('create', {'qID': random_id}, room=room)
+    return {'response': constants.SUCCESS, 'qID': random_id}
 
 
-@socketio.on('join')
+@socketio.on(constants.JOIN)
 def handle_join(data):
     # print(data)
     qID = data['qID']
-    room = qID
-    displayName = data['displayName']
+    displayName = data['data']
 
-    queue_info = get(qID)
+    queue_info = get('queues', qID)
 
     if (queue_info == None):
-        # TODO: change errco to be a dictionary to keep it consistent
-        emit('join', 'ERRCO 1: ROOM DOES NOT EXIST')
-        return
+        return {'response': constants.QID_DOES_NOT_EXIST}
 
-    queue_info = json.loads(queue_info)
-    # print(queue_info)
+    for user in queue_info['connected_users']:
+        if (user['displayName'] == displayName):
+            return {'response': constants.DISPLAY_NAME_NOT_UNIQUE}
 
-    queue_info['connected_users'].append({
+    current_user = {
         'sid': request.sid,
         'displayName': displayName
-        })
+        }
+    queue_info['connected_users'].append(current_user)
 
-    set(qID, json.dumps(queue_info))
+    usersArr = []
+    for user in queue_info['connected_users']:
+        usersArr.append(user['displayName'])
 
-    join_room(room)
-    emit('join', {'displayName': displayName}, room=room, include_self=False)
-    emit('join', queue_info)
+    arrAppend('queues', qID + '.connected_users', current_user)
 
-
-@socketio.on('leave')
+    join_room(qID)
+    emit(constants.USERJOINED, {'data': displayName, 'response': constants.SUCCESS}, room=qID, include_self=False)
+    return {'response': constants.SUCCESS, 'data': usersArr}
+ 
+ 
+@socketio.on(constants.LEAVE)
 def handle_leave(data):
-    # print(data)
     qID = data['qID']
-    room = qID
-    displayName = data['displayName']
+    displayName = data['data']
 
-    queue_info = get(qID)
+    queue_info = get('queues', qID)
     if (queue_info == None):
-        # TODO: change errco to be a dictionary to keep it consistent
-        emit('leave', 'ERRCO 1: ROOM DOES NOT EXIST')
-        return
+        return {'response': constants.QID_DOES_NOT_EXIST}
 
-    queue_info = json.loads(queue_info)
-
-    queue_info['connected_users'].remove({'sid': request.sid, 'displayName': displayName})
-
-    set(qID, json.dumps(queue_info))
+    current_user = {'sid': request.sid, 'displayName': displayName}
+    for i in range(queue_info['connected_users'].length):
+        if queue_info['connected_users'][i] == current_user:
+            tryDatabaseCommand('json.arrpop', 'queues', qID + '.connected_users', i)
+            break
 
     # include_self set to True because the website should wait till we've confirmed
     # that the user has been removed...
-    emit('leave', {'displayName': displayName}, room=room, include_self=True)
-    leave_room(room)
+    leave_room(qID)
+    emit(constants.USERLEFT, {'data': displayName, 'response': constants.SUCCESS}, room=qID, include_self=False)
+    return {'response': constants.SUCCESS}
 
 
-@socketio.on('addToQueue')
-def handle_add_to_queue(data):
-    # print(data)
-
-    # should probably check if the user exists in that qID
+@socketio.on(constants.ADDMEDIA)
+def handle_add_media(data):
     qID = data['qID']
-    room = qID
 
-    # should probably add some error handling...
-    queue_info = json.loads(get(qID))
-    queue_info['queue'].append(data['rowData'])
-    set(qID, json.dumps(queue_info))
+    queue_info = get('queues', qID)
+    if (queue_info == None):
+        return {'response': constants.QID_DOES_NOT_EXIST}
 
-    # should include_self be set to true here?
-    emit('addToQueue', data['rowData'], room=room, include_self=True)
+    media = data['data']
+    mediaId = [*media][0]
+    rowData = media[mediaId]
 
+    set('queues', 'queue.' + mediaId, rowData)
 
-@socketio.on('disconnect')
-def handle_disconnect():
-    for room in rooms():
-        if (room != request.sid):
-            queue_info = json.loads(get(room))
-            for connected_user in queue_info['connected_users']:
-                if (connected_user['sid'] == request.sid):
-                    displayName = connected_user['displayName']
-                    emit('leave', {'displayName': displayName}, room=room, include_self=False)
-                    queue_info['connected_users'].remove(connected_user)
-
-            leave_room(room)
-
-
-# should probably not be used since the user already grabs current queue when they join
-@socketio.on('getQueueInfo')
-def handle_get_queue(data):
-    # print(data)
-    qID = data['qID']
-    queue_info = json.loads(get(qID))
-    emit('getQueueInfo', queue_info['queue'])
-
-
-
+    emit(constants.MEDIAADDED, {'data': {mediaId: rowData}, 'response': constants.SUCCESS}, room=qID, include_self=False)
+    return {'response': constants.SUCCESS}
+# 
+# 
+# @socketio.on('disconnect')
+# def handle_disconnect():
+#     for room in rooms():
+#         if (room != request.sid):
+#             queue_info = json.loads(get(room))
+#             for connected_user in queue_info['connected_users']:
+#                 if (connected_user['sid'] == request.sid):
+#                     displayName = connected_user['displayName']
+#                     emit('leave', {'displayName': displayName}, room=room, include_self=False)
+#                     queue_info['connected_users'].remove(connected_user)
+# 
+#             leave_room(room)
+# 
+# 
+# # should probably not be used since the user already grabs current queue when they join
+# @socketio.on('getQueueInfo')
+# def handle_get_queue(data):
+#     # print(data)
+#     qID = data['qID']
+#     queue_info = json.loads(get(qID))
+#     emit('getQueueInfo', queue_info['queue'])
+# 
+# 
+# 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80, debug=False)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True)
